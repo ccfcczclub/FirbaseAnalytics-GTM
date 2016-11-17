8d026b4c7269e85268e003b0adfdbfc462efcea4
Kamonchanok <kamonchanok.m0592@gmail.com>
2016-11-17 11:35:00 +0700

    no message

--- AnalyticsExample/PatternTabBarController.m
+++ AnalyticsExample/PatternTabBarController.m
@@ -52,7 +52,12 @@
                                         cancelButtonTitle:@"Ok"
                                         otherButtonTitles:nil];
     
-  [FIRAnalytics logEventWithName:@"clickShare" parameters:@{@"share_name":title }];
+  [FIRAnalytics logEventWithName:@"clickShare" parameters:@{@"share_name":title ,
+                                                            @"sso_id":@"00000003",
+                                                            @"client_id":@"client_id_03",
+                                                            @"FB_id":@"ccfcczclub02",
+                                                            @"lat_lng":@"14.1212,9.33",
+                                                            @"Device_id":@"044444"}];
   [alert show];
 }
 
